5f396fb77e07348289a61a234bd24a8a54b3bfda
MaximeDzN <maxime.dazin@gmail.com>
2022-01-10 14:45:47 +0100

    beans xml

---
+++ beansXml/src/main/java/eu/ensup/beansXml/dao/MusiqueDao.java
@@ -0,0 +1,46 @@
+package eu.ensup.beansXml.dao;
+
+import eu.ensup.beansXml.domaine.Musique;
+
+import java.util.Set;
+
+public class MusiqueDao implements IDao{
+
+    @Override
+    public Musique getMusiqueById(int id) {
+        if(id == 1){
+            return new Musique(1,"Maxime chambroncel","moi","toi");
+        }
+        return null;
+    }
+
+    @Override
+    public void createMusique(Musique musique) {
+        System.out.println("Musique créée: "+musique.toString());
+    }
+
+    @Override
+    public Musique updateMusique(Musique musique) {
+        System.out.println("Musique modifiée: "+musique.toString());
+        return musique;
+    }
+
+    @Override
+    public void deleteMusique(Musique musique) {
+        System.out.println("Compte supprimée: "+musique.toString());
+    }
+
+    @Override
+    public Set<Musique> getAllMusique() {
+        System.out.println("Récupération de toutes les ziks");
+        return null;
+    }
+
+    private void initialisation() {
+        System.out.println("DAO : INIT");
+    }
+
+    private void destruction() {
+        System.out.println("DAO : DESTRUCT");
+    }
+}
---
+++ beansXml/src/main/java/eu/ensup/beansXml/launcher/Launcher.java
@@ -0,0 +1,38 @@
+package eu.ensup.beansXml.launcher;
+
+import eu.ensup.beansXml.dao.IDao;
+import eu.ensup.beansXml.dao.MusiqueDao;
+import eu.ensup.beansXml.dao.MusiqueDaoAlt;
+import eu.ensup.beansXml.domaine.Musique;
+import eu.ensup.beansXml.services.MusiqueService;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Launcher {
+
+    public static void main(String[] args) {
+
+        //1. Chargement
+        ConfigurableApplicationContext applicationContext = (ConfigurableApplicationContext) new ClassPathXmlApplicationContext(
+                "prod-beans.xml");
+
+        //2. Récupération
+        MusiqueService musiqueService = (MusiqueService) applicationContext.getBean("musiqueService");
+
+        //3. Manipulation
+        Musique maZikMu = new Musique(5, "Moi", "Flavien", "Le temps des tempêtes");
+
+        musiqueService.enregistrerMusique(maZikMu);
+
+        Musique musique = musiqueService.trouverMusique(1);
+        if (musique != null) {
+            System.out.println("yes");
+        } else {
+            System.out.println("no");
+        }
+
+        applicationContext.close();
+
+    }
+
+}
